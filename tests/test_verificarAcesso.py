7c05b4e0e9615627e5e9d09f19bc431357627700
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2024-05-22 20:28:46 -0300

    test: :white_check_mark: Testes para verificação de acesso
    Acrescentados testes para verificação de acesso do sistema

---
+++ tests/test_verificarAcesso.py
@@ -0,0 +1,50 @@
+import pytest
+
+from ControleAcesso import Usuarios, Funcionalidades, FuncionalidadeUsuarios, SistemaGerenciamentoAcesso
+
+@pytest.fixture
+def sistema_gerenciamento_acesso():
+    return SistemaGerenciamentoAcesso()
+
+@pytest.fixture
+def usuario_exemplo():
+    return Usuarios(id=1, nome="Exemplo", username="exemplo", email="exemplo@example.com", setor="TI", cargo="Desenvolvedor", nivel=3)
+
+@pytest.fixture
+def funcionalidade_exemplo():
+    return Funcionalidades(id=1, nome="Funcionalidade Exemplo", nivel=3)
+
+@pytest.fixture
+def funcionalidade_usuario_exemplo():
+    return FuncionalidadeUsuarios(id=1, idUsuario=1, idFuncionalidade=1, liberado=False)
+
+def test_verificar_acesso_sucesso(sistema_gerenciamento_acesso, usuario_exemplo, funcionalidade_exemplo, funcionalidade_usuario_exemplo):
+    sistema_gerenciamento_acesso.cadastrarUsuario(usuario_exemplo)
+    sistema_gerenciamento_acesso.cadastrarFuncionalidade(funcionalidade_exemplo)
+    sistema_gerenciamento_acesso.associarFuncionalidadeUsuarios(funcionalidade_usuario_exemplo)
+
+    assert sistema_gerenciamento_acesso.verificarAcesso(1, 1) == True
+
+def test_verificar_acesso_falha(sistema_gerenciamento_acesso, usuario_exemplo, funcionalidade_exemplo, funcionalidade_usuario_exemplo):
+    sistema_gerenciamento_acesso.cadastrarUsuario(usuario_exemplo)
+    sistema_gerenciamento_acesso.cadastrarFuncionalidade(funcionalidade_exemplo)
+
+    assert sistema_gerenciamento_acesso.verificarAcesso(1, 1) == False
+
+def test_verificar_acesso_usuario_inexistente(sistema_gerenciamento_acesso, funcionalidade_exemplo):
+    sistema_gerenciamento_acesso.cadastrarFuncionalidade(funcionalidade_exemplo)
+
+    assert sistema_gerenciamento_acesso.verificarAcesso(1, 1) == False
+
+def test_verificar_acesso_funcionalidade_inexistente(sistema_gerenciamento_acesso, usuario_exemplo):
+    sistema_gerenciamento_acesso.cadastrarUsuario(usuario_exemplo)
+
+    assert sistema_gerenciamento_acesso.verificarAcesso(1, 1) == False
+
+def test_verificar_acesso_nivel_funcionalidade_menor(sistema_gerenciamento_acesso, usuario_exemplo, funcionalidade_exemplo, funcionalidade_usuario_exemplo):
+    funcionalidade_exemplo.nivel = 2
+    sistema_gerenciamento_acesso.cadastrarUsuario(usuario_exemplo)
+    sistema_gerenciamento_acesso.cadastrarFuncionalidade(funcionalidade_exemplo)
+    sistema_gerenciamento_acesso.associarFuncionalidadeUsuarios(funcionalidade_usuario_exemplo)
+
+    assert sistema_gerenciamento_acesso.verificarAcesso(1, 1) == False

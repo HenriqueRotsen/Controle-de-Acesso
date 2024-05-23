3e2ec56deb0df5fb727c00ad9f9949ee993f6191
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2024-05-23 20:47:03 -0300

    docs: :memo: Documentação do código

--- Calculadora.py
+++ Calculadora.py
@@ -1,18 +1,75 @@
-def soma(num1, num2):
+def soma(num1: float, num2: float) -> float:
+    """
+    Retorna a soma de dois números.
+
+    Args:
+        num1 (float): O primeiro número.
+        num2 (float): O segundo número.
+
+    Returns:
+        float: A soma de num1 e num2.
+    """
     return num1 + num2
 
-def subtracao(num1, num2):
+def subtracao(num1: float, num2: float) -> float:
+    """
+    Retorna a subtração de dois números.
+
+    Args:
+        num1 (float): O primeiro número.
+        num2 (float): O segundo número.
+
+    Returns:
+        float: A diferença entre num1 e num2.
+    """
     return num1 - num2
 
-def multiplicacao(num1, num2):
+def multiplicacao(num1: float, num2: float) -> float:
+    """
+    Retorna a multiplicação de dois números.
+
+    Args:
+        num1 (float): O primeiro número.
+        num2 (float): O segundo número.
+
+    Returns:
+        float: O produto de num1 e num2.
+    """
     return num1 * num2
 
-def divisao(num1, num2):
+def divisao(num1: float, num2: float) -> float:
+    """
+    Retorna a divisão de dois números.
+
+    Args:
+        num1 (float): O numerador.
+        num2 (float): O denominador.
+
+    Returns:
+        float: O quociente de num1 dividido por num2.
+
+    Raises:
+        ZeroDivisionError: Se num2 for zero.
+    """
     if num2 == 0:
         raise ZeroDivisionError("Erro: Divisão por zero!")
     return num1 / num2
 
 def calculadora():
+    """
+    Interface de linha de comando para uma calculadora simples que realiza
+    operações básicas: adição, subtração, multiplicação e divisão.
+
+    Solicita ao usuário que escolha uma operação e insira dois números para
+    realizar a operação escolhida. O resultado da operação é exibido no console.
+
+    Operações disponíveis:
+        1- Adição
+        2- Subtração
+        3- Multiplicação
+        4- Divisão
+        0- Sair
+    """
     print("\n--- CALCULADORA ---\n")
     print("Escolha a operação:")
     print("1- Adição")
@@ -30,12 +87,15 @@ def calculadora():
     num2 = float(input("Digite o segundo número: "))
 
     if operacao == "1":
-        soma(num1, num2)
+        print(f"Resultado: {soma(num1, num2)}")
     elif operacao == "2":
-        subtracao(num1, num2)
+        print(f"Resultado: {subtracao(num1, num2)}")
     elif operacao == "3":
-        multiplicacao(num1, num2)
+        print(f"Resultado: {multiplicacao(num1, num2)}")
     elif operacao == "4":
-        divisao(num1, num2)
+        try:
+            print(f"Resultado: {divisao(num1, num2)}")
+        except ZeroDivisionError as e:
+            print(e)
     else:
         print("Operação inválida. Tente novamente.")
--- ControleAcesso.py
+++ ControleAcesso.py
@@ -2,12 +2,23 @@ from dataclasses import dataclass
 from Calculadora import calculadora
 
 # Variavel global de número de usuario
-contadorId = 0
-contadorFunc = 0
+CONTADOR_ID = 0
 
 @dataclass
 class Usuarios:
-    def __init__(self, id, nome, username, email, setor, cargo, nivel, senha=123):
+    """Dataclass representativa de usuários do sistema. 
+
+    Args:
+        id (int): Id do usuário. Definido automaticametne pelo sistema
+        nome (str): Nome real de usuário
+        username (str): Nome de usuário mostrado no sistema
+        email (str): Endereço de e-mail do usuário
+        setor (str): Setor do usuário
+        cargo (str): Cargo do usuário (comum ou administrador)
+        nivel (int): Nível de acesso do usuário
+        senha (int, optional): Senha de acesso do usuário ao sistema. Default é 123.
+    """
+    def __init__(self, id: int, nome: str, username: str, email: str, setor:str, cargo:str, nivel: int, senha=123):
         self.id = id
         self.nome = nome
         self.username = username
@@ -19,57 +30,138 @@ class Usuarios:
 
 @dataclass
 class Funcionalidades:
-    def __init__(self, id, nome, nivel):
+    """Definição de funcionalidade do sistema 
+
+    Args:
+        id (int): _description_
+        nome (str): _description_
+        nivel (int): _description_
+    """
+    def __init__(self, id: int, nome: str, nivel: str):
         self.id = id
         self.nome = nome
         self.nivel = nivel
 
-@dataclass    
+
+@dataclass
 class FuncionalidadeUsuarios:
-    def __init__(self, id, idUsuario, idFuncionalidade, liberado):
+    """
+    Classe para representar a associação de funcionalidades com usuários.
+
+    Attributes:
+        id (int): Identificador único da associação.
+        idUsuario (int): Identificador do usuário.
+        idFuncionalidade (int): Identificador da funcionalidade.
+        liberado (bool): Indica se a funcionalidade está liberada para o usuário.
+    """
+    id: int
+    idUsuario: int
+    idFuncionalidade: int
+    liberado: bool
+
+    def __init__(self, id: int, idUsuario: int, idFuncionalidade: int, liberado: bool) -> None:
+        """
+        Inicializa a classe FuncionalidadeUsuarios com os parâmetros fornecidos.
+
+        Args:
+            id (int): Identificador único da associação.
+            idUsuario (int): Identificador do usuário.
+            idFuncionalidade (int): Identificador da funcionalidade.
+            liberado (bool): Indica se a funcionalidade está liberada para o usuário.
+        """
         self.id = id
         self.idUsuario = idUsuario
         self.idFuncionalidade = idFuncionalidade
         self.liberado = liberado
 
+
 class SistemaGerenciamentoAcesso:
+    """
+    Classe para gerenciar o sistema de acesso de usuários e suas funcionalidades.
+
+    Attributes:
+        usuarios (list): Lista de usuários cadastrados.
+        funcionalidades (list): Lista de funcionalidades disponíveis.
+        funcionalidadeUsuarios (list): Lista de associações entre usuários e funcionalidades.
+    """
+
     def __init__(self):
+        """
+        Inicializa a classe SistemaGerenciamentoAcesso com listas vazias para
+        usuários, funcionalidades e associações de funcionalidades com usuários.
+        """
         self.usuarios = []
         self.funcionalidades = []
         self.funcionalidadeUsuarios = []
-    
+
     def verificarSenha(self, userName, senha):
+        """
+        Verifica se a senha fornecida corresponde ao usuário especificado.
+
+        Args:
+            userName (str): Nome de usuário.
+            senha (str): Senha do usuário.
+
+        Returns:
+            tuple: Um par (bool, int) onde o booleano indica se a senha está correta
+            e o inteiro é o ID do usuário, ou -1 se a senha estiver incorreta.
+        """
         for user in self.usuarios:
             if user.username == userName and user.senha == int(senha):
                 return (True, user.id)
         return (False, -1)
-    
+
     def cadastrarUsuario(self, usuario):
+        """
+        Cadastra um novo usuário e associa automaticamente funcionalidades baseadas no nível do usuário.
+
+        Args:
+            usuario (Usuario): O objeto usuário a ser cadastrado.
+        """
         self.usuarios.append(usuario)
         
-        # Associar automaticamente as funcionalidades com base no nível de acesso do usuário
         for funcionalidade in self.funcionalidades:
             if usuario.nivel >= funcionalidade.nivel:
-                funcUsuario = FuncionalidadeUsuarios(id=len(self.funcionalidadeUsuarios) + 1,
-                                                      idUsuario=usuario.id,
-                                                      idFuncionalidade=funcionalidade.id,
-                                                      liberado=True)
-                self.funcionalidadeUsuarios.append(funcUsuario)
+                funcUsuario = FuncionalidadeUsuarios(
+                    id=len(self.funcionalidadeUsuarios) + 1,
+                    idUsuario=usuario.id,
+                    idFuncionalidade=funcionalidade.id,
+                    liberado=True
+                )
             else:
-                funcUsuario = FuncionalidadeUsuarios(id=len(self.funcionalidadeUsuarios) + 1,
-                                                      idUsuario=usuario.id,
-                                                      idFuncionalidade=funcionalidade.id,
-                                                      liberado=False)
-                self.funcionalidadeUsuarios.append(funcUsuario)                
+                funcUsuario = FuncionalidadeUsuarios(
+                    id=len(self.funcionalidadeUsuarios) + 1,
+                    idUsuario=usuario.id,
+                    idFuncionalidade=funcionalidade.id,
+                    liberado=False
+                )
+            self.funcionalidadeUsuarios.append(funcUsuario)                
 
     def associarFuncionalidadeUsuarios(self, funcionalidadeUsuarios):
+        """
+        Associa uma funcionalidade a um usuário.
+
+        Args:
+            funcionalidadeUsuarios (FuncionalidadeUsuarios): A associação a ser adicionada.
+        """
         self.funcionalidadeUsuarios.append(funcionalidadeUsuarios)
 
     def verificarAcesso(self, id_usuario, id_funcionalidade):
+        """
+        Verifica se um usuário tem acesso a uma determinada funcionalidade.
+
+        Args:
+            id_usuario (int): ID do usuário.
+            id_funcionalidade (int): ID da funcionalidade.
+
+        Returns:
+            bool: True se o usuário tiver acesso, False caso contrário.
+        """
         for usuario in self.usuarios:
             if usuario.id == id_usuario:
                 for funcionalidadeUsuarios in self.funcionalidadeUsuarios:
-                    if funcionalidadeUsuarios.idUsuario == id_usuario and funcionalidadeUsuarios.idFuncionalidade == id_funcionalidade:
+                    if (funcionalidadeUsuarios.idUsuario == id_usuario and
+                            funcionalidadeUsuarios.idFuncionalidade == id_funcionalidade):
                         for funcionalidade in self.funcionalidades:
                             if funcionalidade.id == id_funcionalidade:
                                 if usuario.nivel <= funcionalidade.nivel:
@@ -79,23 +171,40 @@ class SistemaGerenciamentoAcesso:
                                     funcionalidadeUsuarios.liberado = False
                                     return False
         return False
-    
+
     def mostrarFuncionalidadesLiberadas(self, id_usuario):
+        """
+        Exibe e retorna as funcionalidades liberadas para um usuário.
+
+        Args:
+            id_usuario (int): ID do usuário.
+
+        Returns:
+            list: Lista de IDs das funcionalidades liberadas.
+        """
         liberadas = []
         for funcUsuario in self.funcionalidadeUsuarios:
             if funcUsuario.idUsuario == id_usuario and funcUsuario.liberado:
-                funcionalidadeAssociada = next((funcionalidade for funcionalidade in self.funcionalidades if funcionalidade.id == funcUsuario.idFuncionalidade), None)
+                funcionalidadeAssociada = next(
+                    (funcionalidade for funcionalidade in self.funcionalidades 
+                     if funcionalidade.id == funcUsuario.idFuncionalidade), None
+                )
                 if funcionalidadeAssociada:
                     liberadas.append(funcionalidadeAssociada.id)
-                    print(str(funcionalidadeAssociada.id) + "- " + funcionalidadeAssociada.nome)
+                    print(f"{funcionalidadeAssociada.id}- {funcionalidadeAssociada.nome}")
         print("0- Sair")
         return liberadas
-        
+
     def novoUsuario(self):
-        #Id definido automaticamente
-        global contadorId
-        contadorId += 1
-        idUsuario = contadorId
+        """
+        Cria e retorna um novo objeto de usuário baseado na entrada do usuário.
+
+        Returns:
+            Usuario: O novo objeto usuário.
+        """
+        global CONTADOR_ID
+        CONTADOR_ID += 1
+        idUsuario = CONTADOR_ID
         
         nomeUsuario = input("Digite o nome do usuário: ")
         usernameUsuario = input("Digite o username do usuário: ")
@@ -116,8 +225,14 @@ class SistemaGerenciamentoAcesso:
 
         return usuario
     
-    def novaFuncionalidade():
-        #Id definido automaticamente
+    def novaFuncionalidade(self):
+        """
+        Cria e retorna uma nova funcionalidade baseada na entrada do usuário.
+
+        Returns:
+            Funcionalidades: A nova funcionalidade.
+        """
+        global contadorFunc
         contadorFunc += 1
         idFuncionalidade = contadorFunc
         
@@ -133,11 +248,26 @@ class SistemaGerenciamentoAcesso:
         return funcionalidade
 
     def atualizarUsuario(self, id_usuario, nome, username, email, setor, cargo, nivel):
+        """
+        Atualiza as informações de um usuário.
+
+        Args:
+            id_usuario (int): ID do usuário a ser atualizado.
+            nome (str): Novo nome do usuário.
+            username (str): Novo username do usuário.
+            email (str): Novo email do usuário.
+            setor (str): Novo setor do usuário.
+            cargo (str): Novo cargo do usuário.
+            nivel (int): Novo nível do usuário.
+
+        Returns:
+            bool: True se a atualização for bem-sucedida, False caso contrário.
+        """
         if not any(usuario.id == id_usuario for usuario in self.usuarios):
             return False
         if any(usuario.username == username and usuario.id != id_usuario for usuario in self.usuarios):
             return False
-    # O ID do usuário existe e o novo username não está duplicado, então atualiza os dados
+        
         for usuario in self.usuarios:
             if usuario.id == id_usuario:
                 if nivel < 0:
@@ -149,14 +279,18 @@ class SistemaGerenciamentoAcesso:
                 usuario.cargo = cargo
                 usuario.nivel = nivel
                 return True
-    # Se o loop terminou sem atualizar, retorna False
         return False
 
     def removerUsuario(self, id_usuario):
+        """
+        Remove um usuário e suas associações de funcionalidades.
+
+        Args:
+            id_usuario (int): ID do usuário a ser removido.
+        """
         self.usuarios = [user for user in self.usuarios if user.id != id_usuario]
         self.funcionalidadeUsuarios = [fu for fu in self.funcionalidadeUsuarios if fu.idUsuario != id_usuario]
         print(f"Usuário {id_usuario} removido com sucesso.")
-
 
 
 if __name__ == "__main__":

6951278ebc24b77817402479180dcb5dce21c10d
Bruno_Henrique <brunohep86@gmail.com>
2024-05-23 21:35:17 -0300

    feat: Teste de remoção de usuário

---
+++ tests/test_removerUsuario.py
@@ -0,0 +1,45 @@
+import pytest
+from ControleAcesso import Usuarios, SistemaGerenciamentoAcesso
+
+@pytest.fixture
+def sistema():
+    return SistemaGerenciamentoAcesso()
+
+
+def test_removerUsuario_sucesso(sistema):
+    usuario = Usuarios(id=1, nome="João", username="joao", email="joao@email.com", setor="RH", cargo="Analista", nivel=1)
+    sistema.cadastrarUsuario(usuario)
+    resultado = sistema.removerUsuario(1)
+
+    # Verifique se o usuário foi removido
+    assert resultado == True
+    assert len(sistema.usuarios) == 0
+
+def test_removerUsuario_inexistente(sistema):
+    usuario = Usuarios(id=1, nome="João", username="joao", email="joao@email.com", setor="RH", cargo="Analista", nivel=1)
+    sistema.cadastrarUsuario(usuario)
+    resultado = sistema.removerUsuario(999)
+
+    # Verifique se o usuário não foi removido
+    assert resultado == False
+    assert len(sistema.usuarios) == 1
+
+def test_removerUsuario_ultimo(sistema):
+    usuario = Usuarios(id=1, nome="João", username="joao", email="joao@email.com", setor="RH", cargo="Analista", nivel=1)
+    sistema.cadastrarUsuario(usuario)
+    resultado = sistema.removerUsuario(1)
+
+    # Verifique se o usuário foi removido
+    assert resultado == True
+    assert len(sistema.usuarios) == 0
+
+def test_removerUsuario_apos_remocao(sistema):
+    usuario = Usuarios(id=1, nome="João", username="joao", email="joao@email.com", setor="RH", cargo="Analista", nivel=1)
+    sistema.cadastrarUsuario(usuario)
+    sistema.removerUsuario(1)
+    resultado = sistema.removerUsuario(1)
+
+    # Verifique se o usuário não foi removido
+    assert resultado == False
+    assert len(sistema.usuarios) == 0
+    
